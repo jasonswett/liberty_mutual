525618359ef064b80e092ead4fe659b629d7ee0b
Jason Swett <jason@benfranklinlabs.com>
2019-04-17 11:17:44 -0400

    Fix bug

--- exercises/day_3/president_spec2.rb
+++ exercises/day_3/president_spec2.rb
@@ -2,7 +2,9 @@ require 'rspec'
 
 RSpec.describe President do
   describe '#name' do
-    president = President.new(first_name: 'John', last_name: 'Adams')
-    expect(president.name).to eq('John Adams')
+    it 'returns the full name' do
+      president = President.new(first_name: 'John', last_name: 'Adams')
+      expect(president.name).to eq('John Adams')
+    end
   end
 end
