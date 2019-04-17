520a3c42a2df0dd856b7d1e339b33e3e377edc1b
Jason Swett <jason@benfranklinlabs.com>
2019-04-17 14:42:00 -0400

    Fix SQL.

--- exercises/day_4/presidents.sql
+++ exercises/day_4/presidents.sql
@@ -1,6 +1,6 @@
 CREATE TABLE presidents (
   id bigserial PRIMARY KEY,
   number int NOT NULL,
-  first_name varchar(5) NOT NULL,
-  last_name varchar(5) NOT NULL
+  first_name varchar(50) NOT NULL,
+  last_name varchar(50) NOT NULL
 );
