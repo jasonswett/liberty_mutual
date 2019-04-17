abd3f26acd456c934ca45e70c548a83b615059d4
Jason Swett <jason@benfranklinlabs.com>
2019-04-17 15:25:38 -0400

    Modify alice_spec.rb.

--- exercises/day_3/alice_spec.rb
+++ exercises/day_3/alice_spec.rb
@@ -4,18 +4,13 @@ class Document
   def initialize(content)
     @content = content
   end
-
-  def word_counts
-    {}
-  end
 end
 
 RSpec.describe Document do
-  describe '#word_counts' do
-    it 'returns word counts' do
-      document = Document.new('apples apples oranges')
-      expect(document.word_counts['apples']).to eq(2)
-      expect(document.word_counts['oranges']).to eq(1)
+  describe '#words' do
+    it 'returns words as an array' do
+      document = Document.new('Apples apples! oranges.')
+      expect(document.words).to eq(['apples', 'apples', 'oranges'])
     end
   end
 end
